93a0e99422cd39d6884a039951ae1709b156094b
B. Heath Robinson <heath@midnighthour.org>
2011-11-04 17:50:52 -0500

    Add template tag
    Add template tag for recording treatment with decorator

--- mvtester/templatetags/mvtester_tags.py
+++ mvtester/templatetags/mvtester_tags.py
@@ -1,7 +1,35 @@
-from django.template.base import Node
+from django.template.base import Node, TemplateSyntaxError
+from mvtester.models import Treatment
+from django import template
+register = template.Library()
 
-class Treat(Node):
-    def __init__(self, measurement):
-        self.measurement = measurement
+GOAL_KEY_TAG = '<input type="hidden" name="mvtester-goal-key" value="{0}:{1}:{2}"/>'
+GOAL_VALUE_TAG = '<input type="hidden" name="{0}:{1}:{2}" value="{3}"/>'
+
+
+class TreatmentNode(Node):
+    def __init__(self, experiment, treatment, goal, default_conversion_status):
+        self.goal = goal
+        self.experiment = experiment
+        self.treatment = treatment
+        self.default_conversion_status = default_conversion_status
     def render(self, context):
-        return '<!--Measuring {0}-->'.format(self.measurement)
+        treatment = Treatment.objects.get(slug=self.treatment)
+        treatment.treat_visitor()
+        goal_key_tag = GOAL_KEY_TAG.format(self.experiment,self.treatment,self.goal)
+        goal_value_tag = GOAL_VALUE_TAG.format(self.experiment,self.treatment,self.goal,self.default_conversion_status)
+        return goal_key_tag + goal_value_tag
+
+
+def treat_visitor(parser, token):
+    try:
+        bits = token.split_contents()
+        exp = bits[1]
+        trt = bits[2]
+        goal = bits[3]
+        conv = bits[4]
+    except ValueError:
+        raise TemplateSyntaxError('The treat_visitor tag takes four arguments.')
+    return TreatmentNode(exp,trt,goal,conv)
+    
+register.tag('treat_visitor',treat_visitor)
